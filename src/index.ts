9edd259dbd37f63ac0ffb1d80880f2731d57faa1
hongfaqiu <951142905@qq.com>
2023-07-04 12:59:28 +0800

    chore: update tiff-imagery-provider

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { ArcGisMapServerImageryProvider, Viewer } from 'cesium';
 import './index.css';
 import TIFFImageryProvider from 'tiff-imagery-provider';
-import proj4 from 'proj4-fully-loaded'; 
+import proj4 from 'proj4-fully-loaded';
 
 const viewer = new Viewer('cesiumContainer', {
   baseLayerPicker: false,
@@ -17,34 +17,33 @@ const viewer = new Viewer('cesiumContainer', {
   orderIndependentTranslucency: false,
 });
 
-const imageryProvider = new ArcGisMapServerImageryProvider({
-  url: 'https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer',
+ArcGisMapServerImageryProvider.fromUrl('https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer', {
   enablePickFeatures: false
-});
-
-viewer.imageryLayers.remove(viewer.imageryLayers.get(0));
-viewer.imageryLayers.addImageryProvider(imageryProvider)
-
-const provider: any = await TIFFImageryProvider.fromUrl('./cogtif.tif', {
-  renderOptions: {
-    single: {
-      colorScale: 'rainbow'
-    }
-  },
-  enablePickFeatures: true,
-  projFunc: (code) => {
-    if (![4326].includes(code)) {
-      try {
-        let prj = proj4(`EPSG:${code}`, "EPSG:4326")
-        if (prj) return prj.forward
-      } catch (e) {
-        console.error(e);
+}).then(async imageryProvider => {
+  viewer.imageryLayers.remove(viewer.imageryLayers.get(0));
+  viewer.imageryLayers.addImageryProvider(imageryProvider)
+  const provider: any = await TIFFImageryProvider.fromUrl('https://sentinel-cogs.s3.us-west-2.amazonaws.com/sentinel-s2-l2a-cogs/56/J/NP/2023/4/S2A_56JNP_20230410_0_L2A/TCI.tif', {
+    enablePickFeatures: true,
+    projFunc: (code) => {
+      if (![4326].includes(code)) {
+        {
+          try {
+            let prj = proj4(`EPSG:${code}`, "EPSG:4326")
+            let unprj = proj4("EPSG:4326", `EPSG:${code}`)
+            if (prj && unprj) return {
+              project: prj.forward,
+              unproject: unprj.forward
+            }
+          } catch (e) {
+            console.error(e);
+          }
+        }
       }
-    }
-  },
-});
+    },
+  });
 
-const imageryLayer = viewer.imageryLayers.addImageryProvider(provider);
-viewer.flyTo(imageryLayer, {
-  duration: 1,
-});
+  const imageryLayer = viewer.imageryLayers.addImageryProvider(provider);
+  viewer.flyTo(imageryLayer, {
+    duration: 1,
+  });
+})
