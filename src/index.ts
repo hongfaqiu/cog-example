dc0c0eda985245b2050a399bfc76e012aef6fcfc
hongfaqiu <951142905@qq.com>
2024-03-06 17:29:15 +0800

    update dep

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
-import { ArcGisMapServerImageryProvider, Viewer } from 'cesium';
+import { ArcGisMapServerImageryProvider, ImageryLayer, Viewer } from 'cesium';
 import './index.css';
 import TIFFImageryProvider from 'tiff-imagery-provider';
 import proj4 from 'proj4-fully-loaded';
 
 const viewer = new Viewer('cesiumContainer', {
+  baseLayer: ImageryLayer.fromProviderAsync(ArcGisMapServerImageryProvider.fromUrl('https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer', {
+    enablePickFeatures: false
+  }), {}),
   baseLayerPicker: false,
   animation: false,
   fullscreenButton: false,
@@ -17,38 +20,32 @@ const viewer = new Viewer('cesiumContainer', {
   orderIndependentTranslucency: false,
 });
 
-ArcGisMapServerImageryProvider.fromUrl('https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer', {
-  enablePickFeatures: false
-}).then(async imageryProvider => {
-  viewer.imageryLayers.remove(viewer.imageryLayers.get(0));
-  viewer.imageryLayers.addImageryProvider(imageryProvider)
-  const provider: any = await TIFFImageryProvider.fromUrl('/cogtif.tif', {
-    enablePickFeatures: true,
-    renderOptions: {
-      single: {
-        colorScale: 'rainbow',
-      }
-    },
-    projFunc: (code) => {
-      if (![4326, 3857, 900913].includes(code)) {
-        {
-          try {
-            let prj = proj4("EPSG:4326", `EPSG:${code}`,)
-            if (prj) return {
-              project: prj.forward,
-              unproject: prj.inverse
-            }
-          } catch (e) {
-            console.error(e);
+const provider: any = await TIFFImageryProvider.fromUrl('/cogtif.tif', {
+  enablePickFeatures: true,
+  renderOptions: {
+    single: {
+      colorScale: 'rainbow',
+    }
+  },
+  projFunc: (code) => {
+    if (![4326, 3857, 900913].includes(code)) {
+      {
+        try {
+          let prj = proj4("EPSG:4326", `EPSG:${code}`,)
+          if (prj) return {
+            project: prj.forward,
+            unproject: prj.inverse
           }
+        } catch (e) {
+          console.error(e);
         }
       }
-      return undefined
-    },
-  });
-
-  const imageryLayer = viewer.imageryLayers.addImageryProvider(provider);
-  viewer.flyTo(imageryLayer, {
-    duration: 1,
-  });
-})
+    }
+    return undefined
+  },
+});
+console.log(provider);
+const imageryLayer = viewer.imageryLayers.addImageryProvider(provider);
+viewer.flyTo(imageryLayer, {
+  duration: 1,
+});
